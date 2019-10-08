83728659e0b7d279d910813993b5ba130aded97a
pschwagler <pschwagl@gmail.com>
2019-10-08 17:28:22 -0400

    added upload form to help with testing

--- src/app.js
+++ src/app.js
@@ -2,29 +2,41 @@ import React from 'react';
 import NavBar from './components/NavBar.jsx';
 import TransactionsList from './components/TransactionsList.jsx';
 import TransactionsGraph from './components/TransactionsGraph.jsx';
+import FileInputForm from './components/FileInputForm.jsx';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			data: [
-				{ date: '01/01/2019', description: 'Skyba', Amount: 49, category: 'Gym' },
-				{ date: '04/04/2019', description: 'Feedfish', Amount: 60, category: 'Restaurants' }
-			]
-		};
-	}
+    this.state = {
+      data: [
+        {
+          date: '01/01/2019',
+          description: 'Skyba',
+          Amount: 49,
+          category: 'Gym'
+        },
+        {
+          date: '04/04/2019',
+          description: 'Feedfish',
+          Amount: 60,
+          category: 'Restaurants'
+        }
+      ]
+    };
+  }
 
-	render() {
-		return (
-			<div>
-				<NavBar />
-				<h1>Hello World</h1>
-				<TransactionsList transactions={this.state.data} />
-				<TransactionsGraph />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <h1>Hello World</h1>
+        <TransactionsList transactions={this.state.data} />
+        <TransactionsGraph />
+        <FileInputForm />
+      </div>
+    );
+  }
 }
 
 export default App;

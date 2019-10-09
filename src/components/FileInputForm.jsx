d421d0b2a9b58e1092566a44919bf6ab43d7d120
pschwagler <pschwagl@gmail.com>
2019-10-08 21:18:23 -0400

    Added express server with routes outlined below. Also added a form that takes in a CSV file and posts to server
    get /    serves static files in dist
    get /api/transactions    gets all transactions
    post /api/file   post csv file that creates transactions

--- src/components/FileInputForm.jsx
+++ src/components/FileInputForm.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import '../formstyle.css';
+import axios from 'axios';
 
-const FileInputForm = () => {
-  return <form>This is the form!</form>;
-};
+class FileInputForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedFile: null
+    };
+
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+    this.onClickHandler = this.onClickHandler.bind(this);
+  }
+
+  onChangeHandler(event) {
+    this.setState({
+      selectedFile: event.target.files[0]
+    });
+  }
+
+  onClickHandler() {
+    const data = new FormData();
+    data.append('file', this.state.selectedFile);
+    axios.post('api/file', data, {}).then(res => {
+      console.log(res.statusText);
+    });
+  }
+
+  render() {
+    return (
+      <form method='post' action='#' id='#'>
+        <div className='form-group files'>
+          <label>Upload Transactions CSV </label>
+          <input type='file' onChange={this.onChangeHandler} />
+        </div>
+        <button type='button' onClick={this.onClickHandler}>
+          Upload
+        </button>
+      </form>
+    );
+  }
+}
 
 export default FileInputForm;

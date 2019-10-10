39d20f0a1460125dcaadc8fc4b78ed3691eafb84
pschwagler <pschwagl@gmail.com>
2019-10-10 15:58:32 -0400

    final commit before presentation

--- src/components/TransactionsList.jsx
+++ src/components/TransactionsList.jsx
@@ -9,123 +9,131 @@ import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
+import Typography from '@material-ui/core/Typography';
 
 const TransactionsList = ({ transactions }) => {
-	const columns = [
-		{ id: 'description', label: 'Description', minWidth: 170 },
-		{ id: 'date', label: 'Date', minWidth: 150 },
-		{ id: 'amount', label: 'Amount', minWidth: 100 },
-		{
-			id: 'category',
-			label: 'Category',
-			minWidth: 170,
-			align: 'right',
-			format: (value) => value.toLocaleString()
-		},
-		{
-			id: 'account',
-			label: 'Account',
-			minWidth: 170,
-			align: 'right'
-		}
-	];
+  const columns = [
+    { id: 'description', label: 'Description', minWidth: 170 },
+    { id: 'date', label: 'Date', minWidth: 150 },
+    { id: 'amount', label: 'Amount', minWidth: 100 },
+    {
+      id: 'category',
+      label: 'Category',
+      minWidth: 170,
+      align: 'right',
+      format: value => value.toLocaleString()
+    },
+    {
+      id: 'account',
+      label: 'Account',
+      minWidth: 170,
+      align: 'right'
+    }
+  ];
 
-	function createData(description, date, amount, category, account) {
-		date = date.toLocaleDateString();
-		return { description, date, amount, category, account };
-	}
+  function createData(description, date, amount, category, account) {
+    date = date.toLocaleDateString();
+    return { description, date, amount, category, account };
+  }
 
-	const rows = transactions.map((transaction) => {
-		return createData(
-			transaction.description,
-			transaction.date,
-			transaction.amount,
-			transaction.category,
-			transaction.account
-		);
-	});
+  const rows = transactions.map(transaction => {
+    return createData(
+      transaction.description,
+      transaction.date,
+      transaction.amount,
+      transaction.category,
+      transaction.account
+    );
+  });
 
-	const useStyles = makeStyles((theme) => ({
-		root: {
-			width: '100%',
-			margin: theme.spacing(2, 0)
-		},
-		tableWrapper: {
-			maxHeight: 440,
-			overflow: 'auto'
-		}
-	}));
+  const useStyles = makeStyles(theme => ({
+    root: {
+      width: '100%',
+      margin: theme.spacing(2, 0)
+    },
+    tableWrapper: {
+      maxHeight: 440,
+      overflow: 'auto'
+    }
+  }));
 
-	const classes = useStyles();
-	const [ page, setPage ] = React.useState(0);
-	const [ rowsPerPage, setRowsPerPage ] = React.useState(10);
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
-	const handleChangePage = (event, newPage) => {
-		setPage(newPage);
-	};
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
-	const handleChangeRowsPerPage = (event) => {
-		setRowsPerPage(+event.target.value);
-		setPage(0);
-	};
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
-	// const handleClick = (e) => {
-	// 	alert('clicked');
-	// };
+  // const handleClick = (e) => {
+  // 	alert('clicked');
+  // };
 
-	return (
-		<Paper className={classes.root}>
-			<div className={classes.tableWrapper}>
-				<Table stickyHeader>
-					<TableHead>
-						<TableRow>
-							{columns.map((column) => (
-								// TODO:
-								<TableCell key={column.id} align={column.align} style={{ minWidth: column.minWidth }}>
-									{column.label}
-								</TableCell>
-							))}
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						{rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-							return (
-								<TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-									{columns.map((column) => {
-										const value = row[column.id];
-										return (
-											<TableCell key={column.id} align={column.align}>
-												{column.format && typeof value === 'number' ? (
-													column.format(value)
-												) : (
-													value
-												)}
-											</TableCell>
-										);
-									})}
-								</TableRow>
-							);
-						})}
-					</TableBody>
-				</Table>
-			</div>
-			<TablePagination
-				rowsPerPageOptions={[ 10, 25, 50, 100 ]}
-				component="div"
-				count={rows.length}
-				rowsPerPage={rowsPerPage}
-				page={page}
-				backIconButtonProps={{
-					'aria-label': 'previous page'
-				}}
-				nextIconButtonProps={{
-					'aria-label': 'next page'
-				}}
-				onChangePage={handleChangePage}
-				onChangeRowsPerPage={handleChangeRowsPerPage}
-			/>
-		</Paper>
-	);
+  return (
+    <Paper className={classes.root}>
+      <Typography variant='h5' component='h4' gutterBottom={true}>
+        <b>All Transactions</b>
+      </Typography>
+      <div className={classes.tableWrapper}>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              {columns.map(column => (
+                // TODO:
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map(row => {
+                return (
+                  <TableRow hover role='checkbox' tabIndex={-1} key={row.code}>
+                    {columns.map(column => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </div>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 50, 100]}
+        component='div'
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        backIconButtonProps={{
+          'aria-label': 'previous page'
+        }}
+        nextIconButtonProps={{
+          'aria-label': 'next page'
+        }}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
 };
 
 export default TransactionsList;
